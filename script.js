a800ca613ebfec20c79fa527712c7174d15f1605
Ahmed <aminedu957@gmail.com>
2024-11-28 15:28:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+// show the menu on click
+const menuicon = document.querySelector('.bx.bx-menu')
+const nav =document.querySelector('.navbar')
+menuicon.addEventListener('click' ,function(){
+nav.classList.toggle('active')
+
+})
